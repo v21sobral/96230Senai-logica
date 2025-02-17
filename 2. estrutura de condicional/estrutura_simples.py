fe262dfb3f2d4c5e2f4185d1921a83748680d349
21 Larbos <49079321+v21sobral@users.noreply.github.com>
2025-02-17 19:41:25 +0000

    salvando aula

---
+++ 2. estrutura de condicional/estrutura_simples.py
@@ -0,0 +1,12 @@
+import os
+os.system("clear")
+
+nota = float(input("Digite sua nota: "))
+
+if nota >= 7:
+    print(f"Sua nota foi: {nota} Parabéns você foi aprovado! ")
+elif nota <= 6.9:
+    print(f"Sua nota foi: {nota} Quase, dá próxima vez estude um pouco mais")
+    elif nota <= 6.9:
+    print(f"Sua nota foi: {nota} Quase, dá próxima vez estude um pouco mais")
+
