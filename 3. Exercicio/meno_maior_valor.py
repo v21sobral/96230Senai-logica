59116ed41bcc66cc3192d35e0f30d76c132809b6
21 Larbos <49079321+v21sobral@users.noreply.github.com>
2025-02-21 04:03:33 +0000

    salvo em ksa

--- 3. Exercicio/meno_maior_valor.py
+++ 3. Exercicio/meno_maior_valor.py
@@ -6,19 +6,26 @@ numero_2 = float(input("Digite o primeiro numero: "))
 soma = (numero_1 + numero_2)
 media = (numero_1 + numero_2)/2
 multiplicacao = (numero_1 * numero_2)
+sair = str
+continuar = str
+entrada = input("")
 
+while entrada != "fim":
+    if numero_1 > numero_2:
+        print(f"O primeiro número que você digitou {numero_1} é o maior número.")
+    elif numero_1 == numero_2:
+        print(f"Os números: {numero_1} e {numero_2} são iguais.")
+    else:
+        print(f"O segundo número que você digitou {numero_2} é o maior número.")
 
+    print(f"A soma dos dois números foi: {soma}")
+    print(f"A multiplicação dos dois números foi: {multiplicacao:.2f}")
+    print(f"A média foi: {media:.2f}")
 
-if numero_1 > numero_2:
-    print(f"Esse é o maior número: {numero_1}")
-elif numero_1 == numero_2:
-    print(f"Os números: {numero_1} e {numero_2} são iguais.")
+    entrada = input("Digite sair para encerrar o programa: ")
+    numero_1 = float(input("Digite o primeiro numero: "))
+    numero_2 = float(input("Digite o primeiro numero: "))
 else:
-    print(f"Esse é o maior número: {numero_2}")
-
-print(f"A soma dos dois números foi: {soma}")
-print(f"A multiplicação dos dois números foi: {multiplicacao}")
-print(f"A média foi: {media:.2f}")
-
+    print("Programa encerrado")
  
 
