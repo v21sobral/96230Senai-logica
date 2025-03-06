97d632c2ce750723730389eeff61654f36973c09
21 Larbos <49079321+v21sobral@users.noreply.github.com>
2025-03-06 23:47:05 +0000

    unifacs

--- extra/aleatório.py
+++ extra/aleatório.py
@@ -1,7 +1,15 @@
 import os
 os.system("clear")
-x = pow(float(input()), float(input("Elevado a: ")))
-while x != 0:
- x = pow(float(input()), float(input("Elevado a: ")))
- print("fim")
-print(x)
+a = float(input("Digite o número que deseja fazer a exponenciação: "))
+
+
+match a:
+    case 0:
+        while a == int :
+            b = float(input("Elevado a: "))
+            x = (a**b)
+            print(f"{a:.0f} elevado a {b:.0f} é igual a {x:.0f}.")
+            a = float(input("\nDigite o número que deseja fazer a exponenciação: "))
+            b = float(input("Elevado a: "))
+
+        print("Programa encerrado.")
