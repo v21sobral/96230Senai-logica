6d4efc89a0bfdd7b56509f28a36d560efd2c4abb
21 Larbos <49079321+v21sobral@users.noreply.github.com>
2025-03-11 04:42:05 +0000

    salvo em ksa

--- 3. Exercicio/Nota_media.py
+++ 3. Exercicio/Nota_media.py
@@ -14,7 +14,7 @@ if media >= 7:
     print(f"Sua média foi: {media:.2f} ")
     print("É isso ai mermão, brocou!")
 elif media < 3:
-    print(f"Sua média foi: {media:.2}f ")
+    print(f"Sua média foi: {media:.2f} ")
     print("Desista só na próxima encarnação.")
 else :
     print(f"{media:.2f} ")
