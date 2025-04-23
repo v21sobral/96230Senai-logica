967cbf3ebd6b09a676e6420c97be0b11fa671344
21 Larbos <49079321+v21sobral@users.noreply.github.com>
2025-04-23 19:53:43 +0000

    23.04.25

--- 8.Arrays/exerc_negativo_zero.py
+++ 8.Arrays/exerc_negativo_zero.py
@@ -2,10 +2,15 @@ import os
 os.system("cls||clear")
 
 lista = []
-for i in range(5):
-    numero = int(input(f"Digite o {i+1}º número: "))
-    if numero < 0:
-        numero = 0  
-    lista.append(numero)
 
-print("Valores no vetor:", lista)
+def vetor():
+    for i in range(5):
+        numero = int(input(f"Digite o {i+1}º número: "))
+        if numero < 0:
+            numero = 0
+        elif numero == 0:
+            print("Você digitou o número 0.")
+        lista.append(numero)
+
+vetor()
+print("Números com negativos igual a 0:", lista)
