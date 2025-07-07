6b27878d49810921dbe8cde8a1fbee9c02e0cf03
21Sobral <v21sobral@gmail.com>
2025-07-07 02:18:11 -0300

    java agora vai

--- 5. Laço de repetição FOR/media5.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system("cls||clear")
-print("Digite suas notas para saber sua média final.\n")
-media = 0
-for nota in range (4):
-    nota = float(input(f" Digite suas {nota + 1}º nota: "))
-    media += nota
-
-print(f"Sua média é: {media / 4}")
---
+++ Inicio.java
@@ -0,0 +1,9 @@
+public class Inicio {
+    public static void main(String[] args) {
+        
+        System.out.println("Simbora embora que a carona chegou!");
+        
+       
+    }   
+    
+}
